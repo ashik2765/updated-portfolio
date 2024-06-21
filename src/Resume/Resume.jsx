3e9c5b36dec64585e8267adb32994c53c6682590
ashik2765 <ashikurrahman2765@gmail.com>
2024-06-21 21:23:18 +0600

    added updated resume

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -2,7 +2,7 @@
 
 const Resume = () => {
     const handleDownload = () => {
-        window.location.href = '/resume.pdf';
+        window.location.href = '/Updated resume.pdf';
     };
     return (
         <button onClick={handleDownload}>
